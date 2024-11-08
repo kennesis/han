96a3c7dafc11647ec5627cd8746379b2a5b37613
Seungmin Yu <kennesis777@gmail.com>
2024-11-08 23:32:19 +0900

    동전 실험

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,3 +1,7 @@
+import { useState, useEffect } from 'react';
+import { IonButton } from '@ionic/react';
+import { Capacitor } from '@capacitor/core';
+import { Haptics } from '@capacitor/haptics';
 import './ExploreContainer.css';
 
 interface ContainerProps {
@@ -5,10 +9,26 @@ interface ContainerProps {
 }
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+  const [ 동전, 동전추가 ] = useState(0);
+  const [ 진동기능, 진동기능전환 ] = useState(false);
+
+  function 동전얻기() {
+    동전추가(동전 + 1);
+    if(진동기능) Haptics.vibrate();
+  }
+
+  useEffect(() => {
+    // 플랫폼 및 기능 확인
+    if (Capacitor.isNativePlatform() && typeof Haptics.vibrate === 'function') {
+      console.log('햅틱 진동 기능을 지원합니다.');
+      진동기능전환(true);
+    }
+  }, []);
+
   return (
     <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <strong>{동전}</strong>
+      <IonButton onClick={() => 동전얻기()}>동전 얻기</IonButton>
     </div>
   );
 };
